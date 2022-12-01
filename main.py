6ae0858364a87ede475b27ac92ff63e9e9cad9a6
ShulzhenkoPetr <petro.shulzhenko@gmail.com>
2022-12-01 16:34:54 +0100

    added main.py with funcs signatures

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def hom_add(cip_1: np.double, cip_2: np.double, encrypts_x: np.array) -> np.double:
+    pass
+
+
+def hom_mult(cip_1: np.double, cip_2: np.double, encrypts_x: np.array) -> np.double:
+    pass
