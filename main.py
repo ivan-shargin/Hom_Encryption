25a6f3a8bf55a0b6d03fb3b3b6cf260b1f845cd2
ShulzhenkoPetr <petro.shulzhenko@gmail.com>
2022-12-02 09:37:52 +0100

    class hierarchy in main.py

--- main.py
+++ main.py
@@ -1,9 +1,41 @@
 import numpy as np
 
 
-def hom_add(cip_1: np.double, cip_2: np.double, encrypts_x: np.array) -> np.double:
-    pass
+class Message:
+    """
+    Encrypted message class
+    """
+
+    def __init__(self, ciphertext):
+        self.value = ciphertext
+
+    def hom_add(self, mes_2):
+        assert isinstance(mes_2, Message)
+        return self.value + mes_2.value
+
+    def hom_mult(self, mes_2, encrypts_x):
+        pass
 
 
-def hom_mult(cip_1: np.double, cip_2: np.double, encrypts_x: np.array) -> np.double:
-    pass
+class Encoder:
+    def __init__(self, key, noise):
+        self.key = key
+        assert noise < 0.5
+        self.noise = noise
+
+    def encode(self, open_message) -> Message:
+        ciphertext = 1
+        return Message(ciphertext)
+
+    def decode(self, ciphertext):
+        return 1
+
+
+if __name__ == '__main__':
+    encoder = Encoder(1, 2)
+    m_1 = 1
+    m_2 = 0
+    message_1 = encoder.encode(m_1)
+    message_2 = encoder.encode(m_2)
+
+    print(encoder.decode(message_1.hom_add(message_2)))
