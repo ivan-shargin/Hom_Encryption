c615ce4bf1a367e8e8e1dae5b2f0f93d79c5bda6
ivan-shargin <shargin.ia@phystech.edu>
2022-12-03 19:41:54 +0300

    added debug mode, fixed key generation - now no keys with all zeros

--- main.py
+++ main.py
@@ -22,15 +22,22 @@ class Encoder:
     def __init__(self, n, noise):
         self.n = n
         self.key = np.random.randint(2, size=n)
+        non_zero_key_ind = np.nonzero(self.key) 
+        if non_zero_key_ind[0].size == 0:
+            i = np.random.randint(n, size=1)[0]
+            self.key[i] = 1 
         assert noise < 0.5
         self.noise = noise
 
-    def encode(self, open_message) -> Message:
+    def encode(self, open_message, debug=False) -> Message:
         ciphertext = np.random.uniform(low=-1.0, high=1.0, size=self.n)
-        print(self.key)
+        
+        if debug:
+            print("Key: {}".format(self.key))
         non_zero_key_ind = np.nonzero(self.key) 
         i = np.random.choice(non_zero_key_ind[0])
-        print(i)        
+        if debug:
+            print("Index of cipher element to compute: {}".format(i))        
         
         ciphertext[i] = 0
         ciphertext[i] = open_message + self.noise - np.dot(ciphertext, self.key)
@@ -38,26 +45,23 @@ class Encoder:
         if ciphertext[i] >= 1:
             ciphertext[i] = ciphertext[i] - 2
         
-        
-        print(ciphertext)
-        print(np.dot(ciphertext, self.key))
+        if debug:
+            print("Ciphertext: {}".format(ciphertext))
+            print("Dot product of ciphertext and key: {}".format(np.dot(ciphertext, self.key)))
         return Message(ciphertext)
 
     def decode(self, message):
-        # print(np.dot(message.value, self.key))
-
-        # print(np.dot(message.value, self.key) % 2)
-        return np.rint(np.dot(message.value, self.key) % 2)
+        return int(np.rint(np.dot(message.value, self.key) % 2))
 
 
 if __name__ == '__main__':
-    encoder = Encoder(5, 0.1)
+    encoder = Encoder(2, 0.1)
     m_1 = 0
-    m_2 = 0
-    message_1 = encoder.encode(m_1)
-    print("decode-open message")
+    m_2 = 1
+    cipher_1 = encoder.encode(m_1, debug=True)
+    cipher_2 = encoder.encode(m_2)
+    print("open message_1 is {}, was decoded as {}".format(m_1, encoder.decode(cipher_1)))
+    print("open message_2 is {}, was decoded as {}".format(m_2, encoder.decode(cipher_2)))
 
-    print(encoder.decode(message_1))
-    #message_2 = encoder.encode(m_2)
-
-    #print(encoder.decode(message_1.hom_add(message_2)))
+    
+    
