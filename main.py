ec0106e82efe997354ac92b11a181169ec365947
ShulzhenkoPetr <petro.shulzhenko@gmail.com>
2022-12-03 15:31:27 +0100

    the first version of encryption and decryption. If you see a bug - tell me

--- main.py
+++ main.py
@@ -14,28 +14,41 @@ class Message:
         return self.value + mes_2.value
 
     def hom_mult(self, mes_2, encrypts_x):
+        assert isinstance(mes_2, Message)
         pass
 
 
 class Encoder:
-    def __init__(self, key, noise):
-        self.key = key
+    def __init__(self, n, noise):
+        self.n = n
+        self.key = np.random.randint(2, size=n)
         assert noise < 0.5
         self.noise = noise
 
     def encode(self, open_message) -> Message:
-        ciphertext = 1
+        ciphertext = np.random.uniform(low=-1.0, high=1.0, size=self.n)
+        i = np.random.randint(self.n)
+        ciphertext[i] = 0
+        print(ciphertext)
+        print(i)
+        print(self.key)
+        print(np.dot(ciphertext, self.key))
+        ciphertext[i] = 2 + open_message + self.noise - np.dot(ciphertext, self.key)
+        print(ciphertext)
         return Message(ciphertext)
 
-    def decode(self, ciphertext):
-        return 1
+    def decode(self, message):
+        print(np.dot(message.value, self.key))
+        print(np.dot(message.value, self.key) % 2)
+        return np.rint(np.dot(message.value, self.key) % 2)
 
 
 if __name__ == '__main__':
-    encoder = Encoder(1, 2)
+    encoder = Encoder(5, 0.1)
     m_1 = 1
     m_2 = 0
     message_1 = encoder.encode(m_1)
-    message_2 = encoder.encode(m_2)
+    print(encoder.decode(message_1))
+    #message_2 = encoder.encode(m_2)
 
-    print(encoder.decode(message_1.hom_add(message_2)))
+    #print(encoder.decode(message_1.hom_add(message_2)))
