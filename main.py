b6f721577d62a772bcdc453896af83d6db982d9d
ivan-shargin <shargin.ia@phystech.edu>
2022-12-04 11:36:37 +0300

    random noise

--- main.py
+++ main.py
@@ -19,23 +19,30 @@ class Message:
 
 
 class Encoder:
-    def __init__(self, n, noise):
+    def __init__(self, n, max_noise):
         self.n = n
+        self.max_noise = max_noise
+        
         self.key = np.random.randint(2, size=n)
-        non_zero_key_ind = np.nonzero(self.key) 
-        if non_zero_key_ind[0].size == 0:
+        self.non_zero_key_ind = np.nonzero(self.key)[0] 
+        if self.non_zero_key_ind.size == 0:
             i = np.random.randint(n, size=1)[0]
             self.key[i] = 1 
-        assert noise < 0.5
-        self.noise = noise
+        self.non_zero_key_ind = np.nonzero(self.key)[0] 
+        
+        assert self.max_noise < 0.5
 
     def encode(self, open_message, debug=False) -> Message:
         ciphertext = np.random.uniform(low=-1.0, high=1.0, size=self.n)
+        self.noise = np.random.uniform(low=0.0, high=self.max_noise)
         
         if debug:
             print("Key: {}".format(self.key))
-        non_zero_key_ind = np.nonzero(self.key) 
-        i = np.random.choice(non_zero_key_ind[0])
+        
+        print("Noise: {}".format(self.noise))
+        
+        i = np.random.choice(self.non_zero_key_ind)
+        
         if debug:
             print("Index of cipher element to compute: {}".format(i))        
         
@@ -48,6 +55,7 @@ class Encoder:
         if debug:
             print("Ciphertext: {}".format(ciphertext))
             print("Dot product of ciphertext and key: {}".format(np.dot(ciphertext, self.key)))
+            
         return Message(ciphertext)
 
     def decode(self, message):
@@ -55,7 +63,7 @@ class Encoder:
 
 
 if __name__ == '__main__':
-    encoder = Encoder(2, 0.1)
+    encoder = Encoder(10, 0.1)
     m_1 = 0
     m_2 = 1
     cipher_1 = encoder.encode(m_1, debug=True)
